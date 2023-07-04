b8b34742eb36f5d6008e45597d9b173619b1090e
Shans-Fun-Time <105845001+Shans-Fun-Time@users.noreply.github.com>
2023-07-04 00:34:39 -0700

    testing

---
+++ CapStoneProject/CapStoneProject/src/main/java/com/example/capstoneproject/CapStoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapStoneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapStoneProjectApplication.class, args);
+    }
+
+}
